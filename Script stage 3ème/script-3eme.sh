258d96a0b5f4885df9cd81b024facc2a8a074e76
François <francois.fedian.4012@free.fr>
2024-10-10 20:25:13 +0200

    Encore une fois, correction d'erreurs bêtes

--- Script stage 3ème/script-3eme.sh
+++ Script stage 3ème/script-3eme.sh
@@ -8,7 +8,7 @@
 check_package()
 {
 	package_name=$1
-	if [ "$(dpkg -l | grep "$package_name" )" = "" ]
+	if [[ "$(dpkg -l | grep "$package_name" )" = "" ]]
 		then
 		install_package "$package_name"
 	fi
@@ -17,14 +17,14 @@ check_package()
 install_package()
 {
 read -p "Le paquet ""$package_name"" n'est pas installé. Voulez-vous l'installer (O/n) ? " answer
-	if [ "$answer" = [Oo*]
+	if [ "$answer" = [[Oo*]]
 		then
 		sudo apt-get install -y "$package_name"
-	elif [ "$answer" = [Nn*]
+	elif [[ "$answer" = [[Nn*]]
 		then
 		echo "Ce paquet étant nécessaire, il est impossible de continuer."
 		exit 0
-	elif ["$answer" = ""
+	elif [[ "$answer" = "" ]]
 		then
 		sudo apt-get install -y "$package_name"
 	else
@@ -51,7 +51,7 @@ sleep 2
 
 # On vérifie si on est bien sous X11
 $session_type=$(echo $XDG_SESSION_TYPE)
-if [[ "$session_type" = "wayland" ]]
+if [[[[ "$session_type" = "wayland" ]]
 then
     echo "Vous n'êtes pas sous Xorg. Il est impossible de continuer. Veuillez vous déconnecter, vous reconnecter sous Xorg et relancer le script."
     exit 1
@@ -62,14 +62,14 @@ check_package gedit
 check_package xdotool
 
 # On vérifie si le fichier cible existe déjà, et si c'est le cas, on le supprime
-if [ -f /tmp/stage-fr]
+if [[ -f /tmp/stage-fr ]]
    then
        rm -f /tmp/stage-fr
 fi
    
-# On ouvre gedit, on tape le texte qui va bien puis on sauvegarde
+# On ouvre mousepad, on tape le texte qui va bien puis on sauvegarde
 echo "Le fichier créé par ce script se trouvera dans /tmp/stage-fr." & sleep 5
-gedit /tmp/stage-fr & sleep 2
+mousepad /tmp/stage-fr & sleep 2
 write_things "Je m'appelle François Ruau,"
 write_things "je suis né le 15/04/2011 à Nantes,"
 write_things "et j'habite à Riaillé."
@@ -78,4 +78,4 @@ write_things "et je vous ai fait ce petit script"
 write_things "pour vous montrer de quoi je suis capable."
 write_things "Vous pouvez me contacter à l'adresse francois.ruau@free.fr."
 xdotool key Control_L+S
-killall gedit
+killall mousepad
