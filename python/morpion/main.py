e7ccfd8dba318622568f125ae297d20a778ed58e
François <francois.fedian.4012@free.fr>
2025-03-22 13:59:36 +0100

    Ajout du système de jeu

--- python/morpion/main.py
+++ python/morpion/main.py
@@ -1,6 +1,8 @@
 import os
 import re
 
+CELL_REGEX = r"^[ABC][123]$"
+
 def clear_screen():
     os.system("cls" if os.name == "nt" else "clear")
 
@@ -11,6 +13,9 @@ def print_grid(grid):
     for i in range(1, 4):
         print(f"{i} | {grid[f'A{i}']} | {grid[f'B{i}']} | {grid[f'C{i}']} |")
         print("  -------------")
+
+def verify_cell(cell):
+    return re.match(r"^[ABC][123]$", cell) is not None 
 
 grid = {
     "A1": " ",
@@ -25,3 +30,15 @@ grid = {
 }
 
 print_grid(grid)
+player = "X"
+
+while True:
+    written_case = input("Dans quelle case voulez-vous jouer ?")
+    if verify_cell(written_case) == False:
+        print("Erreur : nom de case invalide.")
+    if grid["case"] == "X" or grid["case"] == "O":
+        print("Erreur : case déjà occupée.")
+    else:
+        grid["case"] = "X" if player == "X" else grid["case"] = "O"
+    player = "O" if player == "X" else "X" # changement de joueur
+    print_grid(grid)
