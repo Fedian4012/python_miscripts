efd6fba5c9d32a5138a170f88ad21e2ccea7b685
François <francois.fedian.4012@free.fr>
2025-05-05 20:22:10 +0200

    Ajout de docstrings

--- echecs/echiquier.py
+++ echecs/echiquier.py
@@ -1,5 +1,8 @@
 class Echiquier:
+    """La classe pour l'échiquier"""
+
     def __init__(self):
+        """Initialise l'échiquier avec les pièces placées à leur position initiale"""
         self.grille = []
 
         for _ in range(8):
@@ -18,6 +21,11 @@ class Echiquier:
         self.afficher()
 
     def afficher(self):
+        """Affiche l'échiquier de manière lisible avec les coordonnées de colonne (a-h) et de ligne (8-1).
+
+        L'affichage utilise des "barres" pour délimiter les cases, avec les numéros de ligne en ordre décroissant
+        (8 en haut, 1 en bas) et les colonnes étiquetées de 'a' à 'h'.
+        """
         lettres_colonnes = ["a", "b", "c", "d", "e", "f", "g", "h"]
 
         # En-tête des colonnes
@@ -30,6 +38,11 @@ class Echiquier:
             print("  +" + "---+" * 8)
 
     def coords_echecs_a_coords_python(self, coords):
+        """Convertit une coordonnée d'échecs (ex : 'a1') en indices Python (ex : (0,7)).
+
+        L'argument 'coords' est une chaîne de caractères de la forme 'a1', 'd5', etc.
+        Cette fonction retourne un tuple avec l'indice de colonne (0 à 7) et l'indice de ligne (0 à 7).
+        """
         colonne = coords[0].lower()
         ligne = coords[1]
 
@@ -40,6 +53,11 @@ class Echiquier:
         return coords_python
 
     def coords_python_a_coords_echecs(self, coords_python):
+        """Convertit des indices Python (ex : (0,7)) en coordonnées d'échecs (ex : 'a1').
+
+        L'argument 'coords_python' est un tuple (colonne, ligne) avec des indices de 0 à 7.
+        Cette fonction retourne une chaîne de caractères représentant la coordonnée d'échecs correspondante.
+        """
         index_colonne, index_ligne = coords_python
 
         # Conversion de l'index de colonne en lettre (de 0 à 7 → 'a' à 'h')
@@ -51,6 +69,3 @@ class Echiquier:
         # Retourner la coordonnée échiquier
         coords_echecs = f"{colonne}{ligne}"
         return coords_echecs
-
-
-plateau = Echiquier()
